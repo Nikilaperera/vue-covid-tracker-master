a9c5ab4b681a24c5c7c5eeff40e2a690fc4d2914
Nikila Banuka Perera <78866528+Nikilaperera@users.noreply.github.com>
2022-02-05 00:27:19 +0530

    chacnged some css

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i> Covid-19 Tracker
+    </div>
+
+    <p>
+      by
+      <a class="text-blue-300" href="https://nikilaperera.me" target="_blank"
+      >Nikila Perera</a
+      >
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+};
+</script>
